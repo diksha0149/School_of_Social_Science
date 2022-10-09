3e595cb0a11d9f029bb4007d5976f770abb931aa
Diksha <dikshapatidar0149@gmail.com>
2022-10-10 03:45:39 +0530

    Time Table section is done , some designing is remaining

---
+++ activities.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School of Social Science</title>
+    <style>
+        .feed{
+            width: 100%;
+            top: 100px;
+           position: absolute;
+        }
+        .feedback_head{
+            font-size: 40px;
+            font-weight: 900;
+            text-align: center;
+        }
+        .feed h3{
+            width: auto;
+            height: 50%;
+            position: relative;
+            margin: 10px 10px 10px 10px;
+            padding: 10px;
+            background: #eee;
+            box-shadow: 0 8px 8px -4px grey;
+            align-items: center;
+            justify-content: center;
+            /* border: 2px solid red; */
+        }
+        .feed a{
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<?php include 'navbar.php';?>
+<div class="feed">
+    <div class="feedback_head">Activities</div>
+<h3>
+<a href="Notice/Activities._2015-16_docx(1).pdf" style="color:black ;" >Activities 2015-16</a>
+</h3>
+<h3>
+<a href="Notice/Activity.2016-17docx(1).pdf" style="color:black;">Activities 2016-17</a>
+</h3>
+<h3>
+<a href="Notice/Activities.2017-18_docx.pdf" style="color:black;">Activities 2017-18</a>
+</h3> 
+ <h3>
+<a href="Notice/Activity-_2018-19.pdf" style="color:black;">Activities 2018-19</a>
+</h3>
+<h3>
+<a href="Notice/Activities2019.pdf" style="color:black;">Activities 2020</a>
+</h3>
+
+</div>
+</body>
+</html>
--- time_table.php
+++ time_table.php
@@ -35,7 +35,9 @@
 .title {
   text-align: center;
 }
-
+.heading{
+  margin-top: 25px;
+}
 .faq {
   max-width: auto;
   margin: 10px;
@@ -49,7 +51,9 @@
 
 .faq-question {
   margin: 15px;
-  height: 15px;
+  height: 10px;
+  font-size: 20px;
+  font-weight: 900;
 }
 
 .faq-toggle {
@@ -72,7 +76,15 @@
   overflow: hidden;
   transition: max-height 0.8s ease-in-out;
 }
+.faq-answer a{
+  text-decoration: none;
+  color: black;
+  margin-left: 20px;
+}
+.faq-answer ul li{
+  padding: 10px;
 
+}
 .faq.active .faq-answer {
   max-height: 300px;
 }
@@ -86,7 +98,8 @@
   <body>
   <?php include 'navbar.php';?>
     <div class="container">
-      <h1 class="title">Frequently Asked Question</h1>
+      <h1 class="title">Time Table</h1>
+      <h2 class="heading">Diploma Courses</h2>
       <div class="faq">
         <h2 class="faq-question">Diploma Courses</h2>
         <div class="faq-toggle">
@@ -99,32 +112,139 @@
             </ul>
         </div>
       </div>
+      <h2 class="heading">Post Graduation Courses</h2>
       <div class="faq">
-        <h2 class="faq-question">Post Graduation Courses</h2>
+        <div class="faq-question">M.A. CLINICAL PSYCHOLOGY</div>
         <div class="faq-toggle">
           <i class="fa-solid fa-angle-down faq-icon"></i>
         </div>
         <div class="faq-answer">
         <ul>
-                <li><a href="TimeTable/diploma.jpg">M.A. CLINICAL PSYCHOLOGY</a></li>
-                <li><a href="pgdiploma.jpg">M.A. POLITICAL SCIENCE</a></li>
-                <li><a href="TimeTable/diploma.jpg">M.A.SOCIOLOGY</a></li>
-                <li><a href="pgdiploma.jpg">M.B.A. PUBLIC ADMINISTRATION and POLICY</a></li>
-                <li><a href="TimeTable/diploma.jpg">M.B.A. RURAL DEVELOPMENT</a></li>
-                <li><a href="pgdiploma.jpg">M.S.W.</a></li>
+                <li><a href="TimeTable/MACP1.jpg">M.A. CLINICAL PSYCHOLOGY SEM 1</a></li>
+                <li><a href="TimeTable/MACP3.jpg">M.A. CLINICAL PSYCHOLOGY SEM 3</a></li>
             </ul>
         </div>
       </div>
+      
       <div class="faq">
-        <h2 class="faq-question">Under Graduate Courses</h2>
+        <h2 class="faq-question">M.A. POLITICAL SCIENCE</h2>
         <div class="faq-toggle">
           <i class="fa-solid fa-angle-down faq-icon"></i>
         </div>
         <div class="faq-answer">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate corporis neque, ab quaerat illum sint enim minima perspiciatis nostrum, tempora, beatae asperiores non iste porro quisquam doloremque minus! Voluptatum hic,
-          ducimus labore vero expedita itaque. Saepe at vel aperiam illum iure dolorem veniam neque quas ipsa corrupti officiis exercitationem, repellendus aspernatur sint qui quasi laudantium enim accusamus laboriosam consequatur vitae!
+        <ul>
+                <li><a href="TimeTable/MA_Pol_1.jpg">MA Political Science  Sem 1</a></li>
+                <li><a href="TimeTable/MA_Pol_3.jpg">MA Political Science Sem 3</a></li>
+            </ul>
         </div>
       </div>
+
+      <div class="faq">
+        <h2 class="faq-question">M.A.SOCIOLOGY</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/MA_sociology_1.jpg">MA Sociology Sem 1</a></li>
+                <li><a href="TimeTable/MA_sociology_3.jpg">MA Sociology Sem 3</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">M.B.A. PUBLIC ADMINISTRATION and POLICY</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/MBA_sem3.jpg">M.B.A. PUBLIC ADMINISTRATION and POLICY SEM 3</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">M.B.A. RURAL DEVELOPMENT</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/MBA_RD_sem3.jpg">M.B.A. PUBLIC ADMINISTRATION and POLICY SEM 3</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">M.S.W.</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/MSW 1 sem.jpg">M.S.W. SEM 1</a></li>
+                <li><a href="TimeTable/MSW 3rd Sem.jpg">M.S.W. SEM 3</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <h2 class="heading">Under Graduation Courses</h2>
+      <div class="faq">
+        <h2 class="faq-question">B.A. GEOGRAPHY</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/B.A. geography 1 sem.jpg">B.A. GEOGRAPHY SEM 1</a></li>
+                <li><a href="TimeTable/B.A. geography 3 sem.jpg">B.A. GEOGRAPHY SEM 3</a></li>
+                <li><a href="TimeTable/B.A. geography 5 sem.jpg">B.A. GEOGRAPHY SEM 5</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">B.A. PSYCHOLOGY</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+        <li><a href="TimeTable/B.A. psychology 1 sem.jpg">B.A. PSYCHOLOGY SEM 1</a></li>
+                <li><a href="TimeTable/B.A. psychology 3 sem.jpg">B.A. PSYCHOLOGY SEM 3</a></li>
+                <li><a href="TimeTable/B.A. psychology 5 sem.jpg">B.A. PSYCHOLOGY SEM 5</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">B.A.SOCIOLOGY</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/BA Sociology 1 sem.jpg">B.A.SOCIOLOGY SEM 1</a></li>
+                <li><a href="TimeTable/BA Sociology 3 sem.jpg">B.A.SOCIOLOGY SEM 3</a></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="faq">
+        <h2 class="faq-question">B.S.W.</h2>
+        <div class="faq-toggle">
+          <i class="fa-solid fa-angle-down faq-icon"></i>
+        </div>
+        <div class="faq-answer">
+        <ul>
+                <li><a href="TimeTable/BSW 1 sem.jpg">B.S.W. SEM 1</a></li>
+                <li><a href="TimeTable/BSW 3 sem.jpg">B.S.W. SEM 3</a></li>
+                <li><a href="TimeTable/BSW 5 sem.jpg">B.S.W. SEM 5</a></li>
+            </ul>
+        </div>
+      </div>
+
     </div>
     <script>
         const faqs = document.querySelectorAll(".faq");
